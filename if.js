63639d09726b72011e6ad424fe67d9662d9fe21c
Arijit Nnadi <arijitnandi240@gmail.com>
2021-10-27 00:06:18 +0530

    Prime no.

---
+++ if.js
@@ -0,0 +1,15 @@
+// write a program to check and print a number is prime or not.
+// node if.js
+
+const num1= 12
+
+
+
+
+if(num1%1===0) {
+    console.log("Prime")
+}else if(num1%num1===0) {
+    console.log("prime")
+}else{
+    console.log("composite")
+}
